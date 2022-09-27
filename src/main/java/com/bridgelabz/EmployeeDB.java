0aa22fad3365a90ee463e65c8c1bbd5cec157a10
“naman-nagwani” <namannagwani@gmail.com>
2022-09-27 17:04:21 +0530

    UC2 Added

---
+++ src/main/java/com/bridgelabz/EmployeeDB.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+
+public class EmployeeDB {
+
+    static ArrayList<EmployeePayroll> employeeDB = new ArrayList<EmployeePayroll>();
+
+    public static ArrayList<EmployeePayroll> getEmployeeDB() {
+        return employeeDB;
+    }
+}
